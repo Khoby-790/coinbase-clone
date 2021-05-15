c4aaff8f8fcd70ae93cd59b73bcb0d0f76c9f3c5
Khoby-790 <ebaidoo79@gmail.com>
2021-05-15 09:17:01 +0000

    --- Auto-Git Commit ---

--- src/pages/HomePage/sections/ThirdSection.jsx
+++ src/pages/HomePage/sections/ThirdSection.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const ThirdSection = () => {
   return (
     <div className="w-full flex py-4 items-center justify-center">
-      <div className="w-2/3">
+      <div className="w-2/3 flex">
         <div className="flex-1 flex flex-col justify-center">
           <h1 className="text-3xl font-medium">Earn up to $28 worth</h1>
           <h1 className="text-3xl font-medium">of crypto</h1>
@@ -20,9 +20,9 @@ const ThirdSection = () => {
           </button>
         </div>
         <div className="flex-1">
-          <div className="bg-white p-3">
+          <div className="bg-white p-3 hover:shadow-md">
             <div className="w-auto">
-              <span>SKALE</span>
+              <span className="text-2xl mr-4">SKALE</span>
               <span>SKL</span>
             </div>
             <div></div>
