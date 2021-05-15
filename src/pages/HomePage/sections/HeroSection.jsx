40f53b5b39ee68871961cd8838bfcc1b345cd7a0
Khoby-790 <ebaidoo79@gmail.com>
2021-05-15 07:57:22 +0000

    --- Auto-Git Commit ---

--- src/pages/HomePage/sections/HeroSection.jsx
+++ src/pages/HomePage/sections/HeroSection.jsx
@@ -5,10 +5,10 @@ import { PhoneSvg, BtcSvg, ArrowSvg } from "../../../components";
 const HeroSection = () => {
   return (
     <div className="h-screen w-full flex flex-row">
-      <div className="flex-1 flex h-full items-center justify-center bg-blue-800">
-        <Link className="flex flex-row">
+      <div className="flex-1 flex h-full items-center">
+        <Link className="flex flex-row items-center">
           <BtcSvg />
-          <span className="ml-2 text-blue-700">Jump start your portfolio</span>
+          <span className="ml-2 text-blue-700 font-semibold text-lg mr-2">Jump start your portfolio</span>
           <ArrowSvg />
         </Link>
       </div>
