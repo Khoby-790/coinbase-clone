6e6a15a41c883a8faee9aaf9de48db53c07bff7a
Khoby-790 <ebaidoo79@gmail.com>
2021-05-15 08:38:51 +0000

    --- Auto-Git Commit ---

--- src/pages/HomePage/sections/TableRatingSection.jsx
+++ src/pages/HomePage/sections/TableRatingSection.jsx
@@ -38,7 +38,7 @@ const TableRatingSection = () => {
                         scope="col"
                         className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
                       >
-                        Title
+                        Name
                       </th>
                       <th
                         scope="col"
