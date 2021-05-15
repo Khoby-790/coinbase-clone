b284e51c70ce344067de337a8b313c64d709ba9d
Khoby-790 <ebaidoo79@gmail.com>
2021-05-15 07:50:53 +0000

    --- Auto-Git Commit ---

--- src/pages/HomePage/sections/HeroSection.jsx
+++ src/pages/HomePage/sections/HeroSection.jsx
@@ -5,7 +5,7 @@ const HeroSection = () => {
   return (
     <div className="h-screen w-full flex flex-row">
       <div className="flex-1 bg-blue-800"></div>
-      <div className="flex-1 bg-blue-500 flex justify-center items-center">
+      <div className="flex-1 h-full flex justify-center items-center pt-10">
         <PhoneSvg />
       </div>
     </div>
