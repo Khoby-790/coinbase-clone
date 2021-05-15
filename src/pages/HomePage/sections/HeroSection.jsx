c218e018b2d3790ed2638cafe6963ac42817d591
Khoby-790 <ebaidoo79@gmail.com>
2021-05-15 07:51:56 +0000

    --- Auto-Git Commit ---

--- src/pages/HomePage/sections/HeroSection.jsx
+++ src/pages/HomePage/sections/HeroSection.jsx
@@ -1,10 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { PhoneSvg } from "../../../components";
 
 const HeroSection = () => {
   return (
     <div className="h-screen w-full flex flex-row">
-      <div className="flex-1 bg-blue-800"></div>
+      <div className="flex-1 bg-blue-800">
+          <Link className="flex flex-row"></Link>
+      </div>
       <div className="flex-1 h-full flex justify-center items-center pt-10">
         <PhoneSvg />
       </div>
