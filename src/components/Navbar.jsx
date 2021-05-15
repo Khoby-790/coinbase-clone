4d57f1536d3242ec8e296b891b1cd85a32de907b
Khoby-790 <ebaidoo79@gmail.com>
2021-05-15 07:43:09 +0000

    --- Auto-Git Commit ---

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,8 +8,13 @@ const Navbar = () => {
         <CoinbaseLogo />
       </div>
       <div className="w-auto flex-1"></div>
-      <div className="w-96">
-          <button className="border-o outline-none">Signin</button>
+      <div className="w-96 flex items-center justify-center">
+        <button className="border-o outline-none hover:text-blue-700 font-medium">
+          Signin
+        </button>
+        <button className="border-o outline-none hover:bg-blue-700 font-medium">
+          Get Started
+        </button>
       </div>
     </div>
   );
