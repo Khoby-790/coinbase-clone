530fa567b845d433d2cc04179302996f81c3552d
Khoby-790 <ebaidoo79@gmail.com>
2021-05-15 07:46:27 +0000

    --- Auto-Git Commit ---

---
+++ src/pages/HomePage/sections/HeroSection.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const HeroSection = () => {
+  return <div className="h-full w-full flex flex-row">
+      <div className="flex-1"></div>
+  </div>;
+};
+
+export default HeroSection;
