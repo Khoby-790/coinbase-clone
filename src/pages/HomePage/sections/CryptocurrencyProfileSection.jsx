24aaf23d0bd7ebab3ad46df9a49df75f4e4f6460
Khoby-790 <ebaidoo79@gmail.com>
2021-05-15 10:01:47 +0000

    --- Auto-Git Commit ---

--- src/pages/HomePage/sections/CryptocurrencyProfileSection.jsx
+++ src/pages/HomePage/sections/CryptocurrencyProfileSection.jsx
@@ -4,11 +4,11 @@ import { Section } from "../../../components";
 const CryptocurrencyProfileSection = () => {
   return (
     <Section>
-      <div className="w-full flex flex-col items-center">
+      <div className="w-full my-4 flex flex-col items-center">
         <h1 className="text-4xl font-medium">
           Create your cryptocurrency portfolio today
         </h1>
-        <span className="text-gray-400 text-lg">
+        <span className="text-gray-400 mt-4 text-lg">
           Coinbase has a variety of features that make it the best place to
           start trading
         </span>
