d969c952a84d4ea1539963ea96ed0ed96dfbc57d
Khoby-790 <ebaidoo79@gmail.com>
2021-05-15 09:09:28 +0000

    --- Auto-Git Commit ---

--- src/pages/HomePage/sections/ThirdSection.jsx
+++ src/pages/HomePage/sections/ThirdSection.jsx
@@ -3,7 +3,12 @@ import React from "react";
 const ThirdSection = () => {
   return (
     <div className="w-full flex items-center justify-center">
-      <div className="w-2/3"></div>
+      <div className="w-2/3">
+        <div className="flex-1 flex flex-col justify-center">
+          <h1 className="text-xl">Earn up to $28 worth</h1>
+        </div>
+        <div className="flex-1"></div>
+      </div>
     </div>
   );
 };
