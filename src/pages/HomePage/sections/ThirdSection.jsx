70e9157b5724fa4559fdd4454889d49c846aee94
Khoby-790 <ebaidoo79@gmail.com>
2021-05-15 09:08:17 +0000

    --- Auto-Git Commit ---

---
+++ src/pages/HomePage/sections/ThirdSection.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ThirdSection = () => {
+  return (
+    <div className="w-full flex items-center justify-center">
+      <div className="w-2/3"></div>
+    </div>
+  );
+};
+
+export default ThirdSection;
