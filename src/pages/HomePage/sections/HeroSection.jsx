f7b54b74cbe7c827c56f68f01557976edbcbc6ae
Khoby-790 <ebaidoo79@gmail.com>
2021-05-15 07:48:39 +0000

    --- Auto-Git Commit ---

--- src/pages/HomePage/sections/HeroSection.jsx
+++ src/pages/HomePage/sections/HeroSection.jsx
@@ -1,10 +1,13 @@
 import React from "react";
+import PhoneSvg from "../../../components/PhoneSvg";
 
 const HeroSection = () => {
   return (
     <div className="h-screen w-full flex flex-row">
       <div className="flex-1 bg-blue-800"></div>
-      <div className="flex-1 bg-blue-500"></div>
+      <div className="flex-1 bg-blue-500 flex justify-center items-center">
+          <PhoneSvg />
+      </div>
     </div>
   );
 };
