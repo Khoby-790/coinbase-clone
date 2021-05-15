c109272fb5366555b8cf5b955bcf817dfa4f042c
Khoby-790 <ebaidoo79@gmail.com>
2021-05-15 09:10:29 +0000

    --- Auto-Git Commit ---

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,12 +1,14 @@
 import React, { Fragment } from "react";
 import HeroSection from "./sections/HeroSection";
 import TableRatingSection from "./sections/TableRatingSection";
+import ThirdSection from "./sections/ThirdSection";
 
 const HomePage = () => {
   return (
     <Fragment>
       <HeroSection />
       <TableRatingSection />
+      <ThirdSection />
     </Fragment>
   );
 };
--- src/pages/HomePage/sections/ThirdSection.jsx
+++ src/pages/HomePage/sections/ThirdSection.jsx
@@ -2,10 +2,11 @@ import React from "react";
 
 const ThirdSection = () => {
   return (
-    <div className="w-full flex items-center justify-center">
+    <div className="w-full flex py-4 items-center justify-center">
       <div className="w-2/3">
         <div className="flex-1 flex flex-col justify-center">
-          <h1 className="text-xl">Earn up to $28 worth</h1>
+          <h1 className="text-3xl font-medium">Earn up to $28 worth</h1>
+          <h1 className="text-3xl font-medium">of crypto</h1>
         </div>
         <div className="flex-1"></div>
       </div>
